dcbdda3ab8802ab45124d01278e49c7ce21eafb8
manikanta201104 <manikanta0224402@gmail.com>
2025-01-18 13:41:49 +0530

    simple

--- src/db/index.js
+++ src/db/index.js
@@ -7,7 +7,7 @@ const connectDB=async()=>{
         const connectionInstance=await mongoose.connect(`${process.env.DB_URI}/${DB_NAME}`);
         console.log(`MongoDB is connected to ${connectionInstance.connection.host}`);
     } catch (error) {
-        console.log("MangoDB connection is FAILED :",error);
+        console.log("MongoDB connection is FAILED :",error);
         process.exit(1);
     }
 }
